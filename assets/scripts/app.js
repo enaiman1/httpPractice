502b30b9a030a1b38ab54850ec45a4695f60e5ab
Ericcentrik <enaiman1@gmail.com>
2020-01-23 20:25:33 -0500

    refactored code and made a function to handle the http request

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,28 +1,33 @@
-const xhr = new XMLHttpRequest();
+
 const listElement = document.querySelector('.posts')
 const postTemplate = document.getElementById('single-post');
 
+function sendHttpRequest(method, url) {
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        //configure get request
+        xhr.open(method, url);
 
-//configure get request
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+        xhr.responseType = 'json'
 
-xhr.responseType = 'json'
-
-xhr.onload = function() {
-    // const listOfPost = JSON.parse(xhr.response)
-    const listOfPost = xhr.response
-    console.log(listOfPost);
-
-    for(const post of listOfPost) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector('h2').textContent=post.title.toUpperCase();
-        postEl.querySelector('p').textContent=post.body;
-        listElement.append(postEl);
-    }
-    
+        xhr.onload = function () {
+            resolve(xhr.response)
+            // const listOfPost = JSON.parse(xhr.response)
+            const listOfPost = xhr.response
+            for (const post of listOfPost) {
+                const postEl = document.importNode(postTemplate.content, true);
+                postEl.querySelector('h2').textContent = post.title.toUpperCase();
+                postEl.querySelector('p').textContent = post.body;
+                listElement.append(postEl);
+            }
+        }
+        xhr.send();
+    });
+    return promise;
 }
 
-xhr.send();
+
+
 
 
 
